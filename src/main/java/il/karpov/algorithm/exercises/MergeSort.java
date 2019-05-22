e4b2cc9fe1840bcc0eb1fe5f1ed7b96e0905f978
Ilya Karpov <i.karpov@cleverdata.ru>
2019-05-22 11:45:14 +0300

    Add MergeSort(Test) classes skeleton

---
+++ src/main/java/il/karpov/algorithm/exercises/MergeSort.java
@@ -0,0 +1,12 @@
+package il.karpov.algorithm.exercises;
+
+public class MergeSort {
+
+    int[] mergeRcr(int[] a, int[] b) {
+        return null;
+    }
+
+    int[] mergeIter(int[] a, int[] b) {
+        return null;
+    }
+}
---
+++ src/test/java/il/karpov/algorithm/exercises/MergeSortTest.java
@@ -0,0 +1,16 @@
+package il.karpov.algorithm.exercises;
+
+import org.testng.annotations.Test;
+
+public class MergeSortTest {
+
+    @Test
+    public void mergeRcr_should_mergeSortedArrays() {
+
+    }
+
+    @Test
+    public void mergeIter_should_mergeSortedArrays() {
+
+    }
+}
