adb83e2aa69d0c4102372c7f5200c74d41acaf56
Ilya Karpov <i.karpov@cleverdata.ru>
2019-05-23 12:17:40 +0300

    Add mergeRcr function impl + some tests

--- src/main/java/il/karpov/algorithm/exercises/MergeSort.java
+++ src/main/java/il/karpov/algorithm/exercises/MergeSort.java
@@ -2,9 +2,43 @@ package il.karpov.algorithm.exercises;
 
 public class MergeSort {
 
-    int[] mergeRcr(int[] a, int[] b) {
-        return null;
+    static int[] mergeRcr(int[] a, int[] b) {
+        if (a.length == 0) {
+            return b;
+        }
+
+        if (b.length == 0) {
+            return a;
+        }
+
+        if (a[0] < b[0]) {
+            return concat(a[0], mergeRcr(tail(a), b));
+        } else {
+            return concat(b[0], mergeRcr(tail(b), a));
+        }
     }
+
+    private static int[] tail(int[] a) {
+        if (a.length == 0) throw new IllegalArgumentException();
+
+        int[] arr = new int[a.length - 1];
+        for (int i = 1; i < a.length; i++) {
+            arr[i - 1] = a[i];
+        }
+        return arr;
+    }
+
+    private static  int[] concat(int element, int[] arr) {
+        int[] a = new int[arr.length + 1];
+        a[0] = element;
+
+        for (int i = 0; i < arr.length; i++) {
+            a[i + 1] = arr[i];
+        }
+
+        return a;
+    }
+
 
     int[] mergeIter(int[] a, int[] b) {
         return null;
--- src/test/java/il/karpov/algorithm/exercises/MergeSortTest.java
+++ src/test/java/il/karpov/algorithm/exercises/MergeSortTest.java
@@ -1,12 +1,35 @@
 package il.karpov.algorithm.exercises;
 
+import java.util.Arrays;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MergeSortTest {
 
-    @Test
-    public void mergeRcr_should_mergeSortedArrays() {
+    @Test(dataProvider = "getMergedArrays")
+    public void mergeRcr_should_mergeSortedArrays(int[] a, int[] b, int[] expected) {
+        Assert.assertTrue(Arrays.equals(MergeSort.mergeRcr(a, b), expected));
+    }
 
+    @DataProvider
+    private static Object[][] getMergedArrays() {
+        return new Object[][] {
+            { new int[0], new int[] { 1 }, new int[] { 1 } },
+            { new int[] { 1 }, new int[0], new int[] { 1 } },
+
+            { new int[] { 1 }, new int[] { 1 }, new int[] { 1, 1 } },
+
+            { new int[] { 1, 2 }, new int[] { 3, 4 }, new int[] { 1, 2, 3, 4 } },
+            { new int[] { 3, 4 }, new int[] { 1, 2 }, new int[] { 1, 2, 3, 4 } },
+
+            { new int[] { 1, 6 }, new int[] { 2, 4 }, new int[] { 1, 2, 4, 6 } },
+            { new int[] { 2, 4 }, new int[] { 1, 6 }, new int[] { 1, 2, 4, 6 } },
+
+            { new int[] { 1, 3 }, new int[] { 2, 4 }, new int[] { 1, 2, 3, 4 } },
+            { new int[] { 2, 4 }, new int[] { 1, 3 }, new int[] { 1, 2, 3, 4 } }
+        };
     }
 
     @Test
