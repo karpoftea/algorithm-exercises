78b284551bf12611b4f278b9861c19821438cf9e
Ilya Karpov <karpoftea@gmail.com>
2019-07-03 23:43:25 +0300

    Add SpinLock implementation + test

---
+++ src/main/java/il/karpov/algorithm/exercises/intrv/concurrency/SpinLock.java
@@ -0,0 +1,51 @@
+package il.karpov.algorithm.exercises.intrv.concurrency;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * <p><b>Problem</b>: create an implementation of simple lock. Implementation must not use 'synchronized' keyword,
+ * 'java.util.concurrent' synchronizers and 'java.util.concurrent.locks'.
+ * It can use 'volatile' keyword and 'java.util.concurrent.atomic.*' classes.
+ * </p>
+ * <p><b>Thanks to</b>: <a href="https://www.ooma.com/">OOMA</a> tech team.</p>
+ */
+public interface SpinLock {
+    /**
+     * Interface for primitive lock: lock can be acquired by a thread if only it is not already acquired by another
+     * thread. If it is acquired by another thread, then thread block on {@link SpinLock#lock()} method until another
+     * thread will unlock it by {@link SpinLock#unlock()} method.
+     */
+    void lock();
+
+    /**
+     * Tries to release lock. If lock is held by another thread, then no effect is expected. If lock is held by
+     * current thread then lock must be release, i.e. another thread can acquire it.
+     * Implementations <em>must</em> enforce the same memory synchronization semantics as provided
+     * by the built-in monitor lock.
+     */
+    void unlock();
+
+
+    class SpinLockImpl implements SpinLock {
+
+        // use negative value because Thread#getId returns only positives
+        private static final long NO_THREAD = -1L;
+
+        private final AtomicBoolean acquired = new AtomicBoolean(false);
+        private final AtomicLong threadId = new AtomicLong(NO_THREAD);
+
+        @Override
+        public void lock() {
+            while (!acquired.compareAndSet(false, true)) ;
+            threadId.set(Thread.currentThread().getId());
+        }
+
+        @Override
+        public void unlock() {
+            if (threadId.get() != Thread.currentThread().getId()) return;
+            acquired.set(false);
+        }
+    }
+
+}
---
+++ src/test/java/il/karpov/algorithm/exercises/intrv/concurrency/SpinLockTest.java
@@ -0,0 +1,64 @@
+package il.karpov.algorithm.exercises.intrv.concurrency;
+
+import il.karpov.algorithm.exercises.intrv.concurrency.SpinLock.SpinLockImpl;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SpinLockTest {
+
+    @Test
+    public void testCriticalSection() throws InterruptedException {
+        SpinLockImpl spinLock = new SpinLockImpl();
+        Counter counter = new Counter();
+        int expectedValue = 10_000_000;
+
+        ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+
+        for (int i = 0; i < expectedValue; i++) {
+            pool.submit(new IncrementTask(counter, spinLock));
+        }
+
+        pool.shutdown();
+        pool.awaitTermination(1, TimeUnit.MINUTES);
+
+        Assert.assertEquals(counter.get(), expectedValue);
+    }
+
+    static class IncrementTask implements Runnable {
+
+        private final Counter counter;
+        private final SpinLock spinLock;
+
+        public IncrementTask(Counter counter, SpinLock spinLock) {
+            this.counter = counter;
+            this.spinLock = spinLock;
+        }
+
+        @Override
+        public void run() {
+            spinLock.lock();
+            try {
+                counter.inc();
+            } finally {
+                spinLock.unlock();
+            }
+        }
+    }
+
+    static class Counter {
+
+        private int value;
+
+        public void inc() {
+            value++;
+        }
+
+        public int get() {
+            return value;
+        }
+    }
+}
